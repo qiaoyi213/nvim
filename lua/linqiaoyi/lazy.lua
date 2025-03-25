67204916f7650de7cf4df51a03e314bdabaccf9b
qiaoyi213 <linqiaoyi213@gmail.com>
2025-03-25 15:55:47 +0800

    remove lsp

--- lua/linqiaoyi/lazy.lua
+++ lua/linqiaoyi/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "linqiaoyi.plugins" }, { import = "linqiaoyi.plugins.lsp" } }, {
+require("lazy").setup({ { import = "linqiaoyi.plugins" } }, {
     checker = {
         enabled = true,
         notify = false,
